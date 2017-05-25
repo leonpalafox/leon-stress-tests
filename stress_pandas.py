276aee1b2b62d9d847539516baef10cd41b3cda9
Leon Palafox <leonoel49@gmail.com>
2017-05-25 12:08:31 -0500

    pdf

--- stress_pandas.py
+++ stress_pandas.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
-
+def sorti(x):
+	a = np.sort(x)
+	return np.mean(a[:4])
 test_df = pd.DataFrame(np.random.rand(10000,10000))
 import timeit
 start_time = timeit.default_timer()
 data_neighbours = pd.DataFrame(index=test_df.columns,columns=range(1,5))
-for i in range(0,len(test_df.columns)):
-    data_neighbours.ix[i,:10] = test_df.ix[0:,i].sort_values(ascending=False)[:4].index
+nr = test_df.apply(lambda x:sorti(x))
 elapsed = timeit.default_timer() - start_time
 print(elapsed)
